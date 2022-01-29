1a53b3e0f4f076b6cee2ad708798cf4362c77e95
Alex M Boulchouk <amboulchouk@outlook.com>
2022-01-28 21:20:38 -0300

    agregados todos los archivos del proyecto

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 
-const app = express()
+const app = express();
 
 app.listen("3000", function(){
     console.log("Holaa Mundo!")
-})
+});
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('*',(req, res) => {
+    res.sendFile(__dirname + '/views/notfound.html')
+});
